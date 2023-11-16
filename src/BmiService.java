7fee1e5024ca471afabfd1449ce0f7075e7d2c13
ustas <ustas-jt@yandex.ru>
2023-11-16 20:04:04 +0300

    Changes were made in the incoming growth data and in the calculation formula

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int weight, int height) {
-        int result = (weight * 10_000 / (height * height));
-        return result;
+    public int calculate(int weight, double height) {
+        double result = (weight / (height * height));
+        return (int) result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 91; // килограмм(ов)
-        int height = 173; // сантиметра(ов)
+        int weight = 75; // килограмм(ов)
+        double height = 1.7; // метра(ов)
         int index = service.calculate(weight, height);
         System.out.println(" bmi-индекс = " + index);
 
